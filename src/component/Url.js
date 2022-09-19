1d9c25bbfd61a176aa4ee9c8de75792b119dd7e3
Parv Singhal <42579742+singhalparv@users.noreply.github.com>
2022-09-20 00:27:08 +0530

    Update Url.js

--- src/component/Url.js
+++ src/component/Url.js
@@ -1,3 +1,3 @@
 // export const url='http://http://springbootmysqlcrudawstweetupdatedog-env.eba-wssxymfw.us-west-2.elasticbeanstalk.com/api/v1.0/tweets'
-export const url='https://cors-everywhere.herokuapp.com/http://springbootmysqlcrudawstweetupdatedog-env.eba-wssxymfw.us-west-2.elasticbeanstalk.com/api/v1.0/tweets'
+export const url='https://cors-everywhere.herokuapp.com/http://springbootmysqlawsfinal-env.eba-cw3jcibj.us-west-2.elasticbeanstalk.com/api/v1.0/tweets'
 //export const url='http://localhost:5000/api/v1.0/tweets'
